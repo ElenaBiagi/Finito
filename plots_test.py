05b4ae2d167ab9d75d495483d199cd33cdeaeb8a
ElenaBiagi <elena.biagi24@gmail.com>
2023-10-17 12:04:07 +0300

    update plots build

--- plots_test.py
+++ plots_test.py
@@ -60,31 +60,31 @@ def do_scatter_plot2(X1, Y1, X2, Y2, X3, Y3, X4, Y4, label1, label2,label3, labe
     plt.savefig(filename)
     
 
-def plot_runs(k3_infile, k2_infile, dataset_tame, outfile_prefix):
+def plot_runs(k3_infile, k2_infile, dataset_name, outfile_prefix):
     k3_t, k3_counts, k3_sumfreq, k3_avgfreq , k3_avglen = parse(k3_infile)
     k2_t, k2_counts, k2_sumfreq, k2_avgfreq, k2_avglen = parse(k2_infile)
 
     do_scatter_plot(k3_t, k3_counts, k2_t, k2_counts,
                     "Shortest", "rarest", "SBWT columns", "# finimizers",
-                    "# finimizers ({})".format(dataset_tame),
+                    "# finimizers ({})".format(dataset_name),
                     "{}_fmin_by_t.pdf".format(outfile_prefix))
 
     do_scatter_plot(k3_t, k3_sumfreq, k2_t, k2_sumfreq,
                     "Shortest", "Rarest", "SBWT columns", "Memory (GB)",
-                    "Memory ({})".format(dataset_tame),
+                    "Memory ({})".format(dataset_name),
                     "{}_sumfreq_by_t.pdf".format(outfile_prefix))
 
     do_scatter_plot(k3_t,k3_avgfreq, k2_counts, k2_avgfreq,
                     "Shortest", "Rarest", "k", "Time (s)",
-                    "Time ({})".format(dataset_tame),
+                    "Time ({})".format(dataset_name),
                     "{}_avgfreq_by_t.pdf".format(outfile_prefix))
 
     do_scatter_plot(k3_t,k3_avglen, k2_counts, k2_avglen,
                     "Shortest", "Rarest", "k", "Time (s)",
-                    "Time ({})".format(dataset_tame),
+                    "Time ({})".format(dataset_name),
                     "{}_avglen_by_t.pdf".format(outfile_prefix))
 
-def plot_runs_allk(k1_infile, k2_infile, k3_infile, k4_infile, dataset_tame, outfile_prefix): 
+def plot_runs_allk(k1_infile, k2_infile, k3_infile, k4_infile, dataset_name, outfile_prefix): 
     k1_t, k1_counts, k1_sumfreq, k1_avgfreq , k1_avglen = parse(k1_infile)
     k2_t, k2_counts, k2_sumfreq, k2_avgfreq, k2_avglen = parse(k2_infile)
     k3_t, k3_counts, k3_sumfreq, k3_avgfreq , k3_avglen = parse(k3_infile)
@@ -92,22 +92,22 @@ def plot_runs_allk(k1_infile, k2_infile, k3_infile, k4_infile, dataset_tame, out
 
     do_scatter_plot2(k1_t, k1_counts, k2_t, k2_counts,k3_t, k3_counts, k4_t, k4_counts,
                     "k=21", "k=31", "k=63", "k=127", "t", "# finimizers",
-                    "Flipped Unitigs {}".format(dataset_tame),
+                    "Flipped Unitigs {}".format(dataset_name),
                     "{}_fmin_by_t.pdf".format(outfile_prefix))
 
     do_scatter_plot2(k1_t, k1_sumfreq, k2_t, k2_sumfreq,k3_t, k3_sumfreq, k4_t, k4_sumfreq,
                     "k=21", "k=31", "k=63", "k=127", "t", "Sum of finimizers frequencies",
-                    "Flipped Unitigs {}".format(dataset_tame),
+                    "Flipped Unitigs {}".format(dataset_name),
                     "{}_sumfreq_by_t.pdf".format(outfile_prefix))
 
     do_scatter_plot2(k1_t,k1_avgfreq, k2_t, k2_avgfreq, k3_t,k3_avgfreq, k4_t, k4_avgfreq,
                    "k=21", "k=31", "k=63", "k=127", "t", "Average finimmizer frequency",
-                    "Flipped Unitigs {}".format(dataset_tame),
+                    "Flipped Unitigs {}".format(dataset_name),
                     "{}_avgfreq_by_t.pdf".format(outfile_prefix))
 
     do_scatter_plot2(k1_t,k1_avglen, k2_t, k2_avglen, k3_t,k3_avglen, k4_t, k4_avglen,
                     "k=21", "k=31", "k=63", "k=127", "t", "Average finimmizer length",
-                    "Flipped Unitigs {}".format(dataset_tame),
+                    "Flipped Unitigs {}".format(dataset_name),
                     "{}_avglen_by_t.pdf".format(outfile_prefix))
 
 
@@ -119,5 +119,5 @@ if not os.path.exists("test_plots"):
 #plot_runs("data_for_plots/k3_metagenome.csv", "data_for_plots/k2_metagenome.csv", "data_for_plots/superalphabet-2_metagenome.csv", "Metagenome reads", "plots/metagenome")
 
 plot_runs_allk("data_for_plots/f_coli3682-unitigs-k21.sbwttest.txt", "data_for_plots/f_coli3682-unitigs-k31.sbwttest.txt","data_for_plots/f_coli3682-unitigs-k63.sbwttest.txt", "data_for_plots/f_coli3682-unitigs-k127.sbwttest.txt", "E. coli", "test_plots/coli_funitigs")
-#plot_runs_allk("data_for_plots/f_ERR5035349-unitigs-k21.sbwttest.txt", "data_for_plots/f_ERR5035349-unitigs-k31.sbwttest.txt","data_for_plots/f_ERR5035349-unitigs-k63.sbwttest.txt", "data_for_plots/f_ERR5035349-unitigs-k127.sbwttest.sbwttest.txt", "Metagenome", "test_plots/metagenome_funitigs")
+plot_runs_allk("data_for_plots/f_ERR5035349-unitigs-k21.sbwttest.txt", "data_for_plots/f_ERR5035349-unitigs-k31.sbwttest.txt","data_for_plots/f_ERR5035349-unitigs-k63.sbwttest.txt", "data_for_plots/f_ERR5035349-unitigs-k127.sbwttest.txt", "Metagenome", "test_plots/metagenome_funitigs")
 plot_runs_allk("data_for_plots/f_SRR25689478-unitigs-k21.sbwttest.txt", "data_for_plots/f_SRR25689478-unitigs-k31.sbwttest.txt","data_for_plots/f_SRR25689478-unitigs-k63.sbwttest.txt", "data_for_plots/f_SRR25689478-unitigs-k127.sbwttest.txt", "E. coli", "test_plots/nanopore_funitigs")
